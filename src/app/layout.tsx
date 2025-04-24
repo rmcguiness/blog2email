83a7eaa9fc6010869fd7cb441f32618958a59c6b
Ryan McGuiness <ryanmcguiness123@gmail.com>
2025-04-24 10:22:21 -0400

    added speed insights to layout:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/NavBar/Navbar";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Blog2Email - Get blog updates in your inbox",
@@ -23,6 +24,7 @@ export default function RootLayout({
           {children}
         </main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
